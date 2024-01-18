9326aa064d18df9daf0254075f76735b1dbd9a5e
Emanuelly Santos <moreiransantos@gmail.com>
2024-01-18 17:09:48 -0300

    primeiro commit

---
+++ desafio-2.js
@@ -0,0 +1,7 @@
+//criar função que recebe quantidade de vitórias e derrotas do jogador
+
+
+//retornar resultado para uma variável, o saldo de rankeadas deve ser feito através do cálculo (vitórias - derrotas)
+
+
+// saída: ao final deve ser exibido a mensagem: "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nível}**"
