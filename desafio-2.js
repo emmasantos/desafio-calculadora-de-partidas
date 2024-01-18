aca2e12596340b90d93af15a3b1ab5c550a9ab85
Emanuelly Santos <moreiransantos@gmail.com>
2024-01-18 18:32:20 -0300

    entrega do desafio

--- desafio-2.js
+++ desafio-2.js
@@ -1,7 +1,31 @@
-//criar função que recebe quantidade de vitórias e derrotas do jogador
+//vitórias e derrotas
+let vitorias = 85
+let derrotas = 26
 
+//função que recebe quantidade de vitórias e derrotas do jogador
+function calcularRank(vitorias, derrotas) {
+    return vitorias - derrotas;
+}
 
-//retornar resultado para uma variável, o saldo de rankeadas deve ser feito através do cálculo (vitórias - derrotas)
+//resultado para variável
+let saldoVitorias = calcularRank(vitorias, derrotas)
 
+if (saldoVitorias <= 10){
+    nivel = "Ferro"
+} else if ((saldoVitorias >= 11) && (saldoVitorias <= 20)){
+    nivel = "Bronze"
+} else if ((saldoVitorias >= 21) && (saldoVitorias <= 50)){
+    nivel = "Prata"
+} else if ((saldoVitorias >= 51) && (saldoVitorias <= 80)){
+    nivel = "Ouro"
+} else if ((saldoVitorias >= 81) && (saldoVitorias <= 90)){
+    nivel = "Diamante"
+} else if ((saldoVitorias >= 91) && (saldoVitorias <= 100)){
+    nivel = "Lendário"
+} else if (saldoVitorias >= 101){
+    nivel = "Imortal"
+}
 
 // saída: ao final deve ser exibido a mensagem: "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nível}**"
+console.log("-------CALCULADORA DE PARTIDAS RANKEADAS-------")
+console.log("O Herói tem saldo de " + saldoVitorias + " e está no nível " + nivel + " !") 
